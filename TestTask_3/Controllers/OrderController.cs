51f763fe5d0d799cc8aebfa4aeb3e6e5b5fcb467
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 15:57:48 +0300

    Commit

--- TestTask_3/Controllers/OrderController.cs
+++ TestTask_3/Controllers/OrderController.cs
@@ -27,5 +27,31 @@ namespace FlowerSaleStore.WebUI.Controllers
             Order order = new Order();
             return View(order);
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToList();
+                repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
     }
 }
