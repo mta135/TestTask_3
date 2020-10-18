874776f537ab49ef7db9ae3cf3648c6c41d11b3e
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 18:17:10 +0300

    add Cart class

---
+++ TestTask_3/Models/Cart.cs
@@ -0,0 +1,54 @@
+﻿using FlowerSalesStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace FlowerSaleStore.WebUI.Models
+{
+    public class Cart
+    {
+        private readonly List<CartLine> lineCollection = new List<CartLine>();
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+            if(line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public virtual void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.Id == product.Id);
+        }
+
+        public virtual decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+
+        public virtual void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public virtual IEnumerable<CartLine> Lines
+        {
+            get
+            {
+                return lineCollection;
+            }
+        }
+
+    }
+}
