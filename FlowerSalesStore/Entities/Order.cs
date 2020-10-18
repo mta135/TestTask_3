e81721187d42c148ece6d2839b2e32f1dc9d21c1
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 19:02:28 +0300

    CartLine.cs, Order.cs

---
+++ FlowerSalesStore/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowerSalesStore.Domain.Entities
+{
+    public class Order
+    {
+        [BindNever]
+        public List<CartLine> Lines { get; set; }
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public string Comment { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
