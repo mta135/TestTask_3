22f42d05615cd147b84266991984ff7b836c744b
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 15:49:34 +0300

    add OrderRepository class, SessionExtensions class, UrlExtensions class

---
+++ TestTask_3/Infrastructure/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using FlowerSalesStore.Domain.Abstract;
+using FlowerSalesStore.Domain.Data;
+using FlowerSalesStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerSaleStore.WebUI.Infrastructure
+{
+    public class OrderRepository : IOrder
+    {
+        private readonly FlowerSaleStoreDbContext context;
+        public OrderRepository(FlowerSaleStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Order> Orders
+        {
+            get
+            {
+                return context.Orders.Include(o => o.Lines).ThenInclude(l => l.Product);
+            }
+        }
+
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.Id == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ TestTask_3/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace FlowerSaleStore.WebUI.Infrastructure
+{
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null ? default : JsonConvert.DeserializeObject<T>(sessionData);
+        }
+    }
+}
