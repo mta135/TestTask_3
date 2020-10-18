810cd64e03c74adb9e6e8e5c70c4841c71e46804
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 19:03:51 +0300

    add IOrder interface

---
+++ FlowerSalesStore/Abstract/IOrder.cs
@@ -0,0 +1,14 @@
+﻿using FlowerSalesStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlowerSalesStore.Domain.Abstract
+{
+    public interface IOrder
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
