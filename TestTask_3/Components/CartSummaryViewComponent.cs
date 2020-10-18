a1dfe1c3211c3ec7e47cf0ff993fea0ed6317cc0
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-18 19:06:18 +0300

    CartSummaryViewComponent

---
+++ TestTask_3/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using FlowerSaleStore.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerSaleStore.WebUI.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart cart;
+        public CartSummaryViewComponent(Cart cart)
+        {
+            this.cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
