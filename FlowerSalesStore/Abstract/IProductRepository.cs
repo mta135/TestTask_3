61d134a9854bc42545d89fe19ac4de1f367ea361
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-19 15:54:01 +0300

    Add methode for SaveProduct and DeleteProduct

--- FlowerSalesStore/Abstract/IProductRepository.cs
+++ FlowerSalesStore/Abstract/IProductRepository.cs
@@ -8,5 +8,9 @@ namespace FlowerSalesStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
     }
 }
