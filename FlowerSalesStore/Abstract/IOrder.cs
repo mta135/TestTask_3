c000e8a005fac8276d73b274b4cfe06da5aeadaa
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-19 12:10:10 +0300

    refactoring of SaveOrder methode

--- FlowerSalesStore/Abstract/IOrder.cs
+++ FlowerSalesStore/Abstract/IOrder.cs
@@ -1,4 +1,5 @@
 ﻿using FlowerSalesStore.Domain.Entities;
+using FlowerSalesStore.Domain.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace FlowerSalesStore.Domain.Abstract
     public interface IOrder
     {
         IQueryable<Order> Orders { get; }
-        void SaveOrder(Order order);
+      //  void SaveOrder(Order order);
+        void SaveOrder(List<OrderViewModel> orderViewModelsList);
     }
 }
