1aacaa6d09d7f1d12e7bfa87530c532018460bf2
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-19 15:55:58 +0300

    Implement SaveProduct and DeleteProduct method

--- TestTask_3/Infrastructure/ProductRepository.cs
+++ TestTask_3/Infrastructure/ProductRepository.cs
@@ -12,7 +12,7 @@ namespace FlowerSaleStore.WebUI.Infrastructure
     public class ProductRepository : IProductRepository
     {
         private FlowerSaleStoreDbContext context;
- 
+
         public ProductRepository(FlowerSaleStoreDbContext context)
         {
             this.context = context;
@@ -20,12 +20,51 @@ namespace FlowerSaleStore.WebUI.Infrastructure
 
         public IEnumerable<Product> Products
         {
-
             get
             {
                 return context.Products.Include(p => p.Category);
             }
-            
+        }
+
+
+
+        public void SaveProduct(Product product)
+        {
+            try
+            {
+                if (product.Id == 0)
+                {
+                    context.Products.Add(product);
+                }
+                else
+                {
+                    Product dbEntry = context.Products.FirstOrDefault(p => p.Id == product.Id);
+                    if (dbEntry != null)
+                    {
+                        dbEntry.Name = product.Name;
+                        dbEntry.Description = product.Description;
+                        dbEntry.Price = product.Price;
+                        //dbEntry.Category = product.Category;
+                    }
+                }
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string exceptions = ex.ToString();
+            }
+        }
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products.FirstOrDefault(p => p.Id == productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
         }
     }
 }
+
