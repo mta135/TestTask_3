757d670ddeb519c290353dad1ed8feb18adfd432
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-19 12:09:13 +0300

    add orederViewModel

---
+++ FlowerSalesStore/ViewModels/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowerSalesStore.Domain.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
