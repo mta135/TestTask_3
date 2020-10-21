dda8dbf4861f6f3e725c0ce08da8091bd6c59974
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-21 15:26:18 +0300

    Commit

---
+++ FlowerSalesStore/Abstract/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using FlowerSalesStore.Domain.Entities;
+using FlowerSalesStore.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlowerSalesStore.Domain.Abstract
+{
+    public interface IOrder
+    {
+        IQueryable<Order> Orders { get; }
+        //  void SaveOrder(Order order);
+        void SaveOrder(List<OrderViewModel> orderViewModelsList);
+    }
+}
