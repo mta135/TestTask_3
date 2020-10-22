9a9138369384207e3dedf5eecc90fd2c5f254c6c
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-22 15:12:54 +0300

    commit

--- FlowerSalesStore/ViewModels/ProductViewModel.cs
+++ FlowerSalesStore/ViewModels/ProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlowerSalesStore.Domain.ViewModels
 {
@@ -7,7 +8,6 @@ namespace FlowerSalesStore.Domain.ViewModels
     {
         public int Id { get; set; }
         public int CategoryId { get; set; } // ?
-
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
