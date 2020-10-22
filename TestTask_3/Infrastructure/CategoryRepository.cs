06caa75831656573f6262d0a1efca287f641ea81
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-22 15:05:40 +0300

    commit

--- TestTask_3/Infrastructure/CategoryRepository.cs
+++ TestTask_3/Infrastructure/CategoryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using FlowerSalesStore.Domain.Abstract;
 using FlowerSalesStore.Domain.Data;
 using FlowerSalesStore.Domain.Entities;
+using FlowerSalesStore.Domain.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,5 +25,22 @@ namespace FlowerSaleStore.WebUI.Infrastructure
                 return context.Categories.ToList();
             }
         }
+
+        //public void SaveCategory(CategoryViewModel categoryView)
+        //{
+        //    try
+        //    {
+        //        Category dbEntry = context.Categories.FirstOrDefault(c => c.Id == categoryView.Id);
+        //        if (dbEntry != null)
+        //        {
+        //            dbEntry.Name = categoryView.Name;
+        //            dbEntry.Description = categoryView.Description;
+        //        }
+        //        context.SaveChanges();
+        //    } catch(Exception ex)
+        //    {
+        //        string exception = ex.ToString();
+        //    }
+        //}
     }
 }
